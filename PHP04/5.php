3c36f5ff14f24a2e78aa7a1b430b444293e6c817
ryuto <n44n7zb3mvangjzykv1m@docomo.ne.jp>
2023-08-17 05:10:25 +0000

    修正箇所にコメントを追加

--- PHP04/5.php
+++ PHP04/5.php
@@ -1,17 +1,18 @@
 <?php
-$calendar_2018 = [
+$calendar_2018 = [       //変数の先頭が数字のため、calendarと2018を入れ替え
   "January" => "1月",
-  "February" => "2月",
+  "February" => "2月",   //、を,に修正
   "March" => "3月",
   "April" => "4月",
   "May" => "5月",
-  "June" => "6月",
+  "June" => "6月",       //=を=>に修正
   "July" => "7月",
   "August" => "8月",
   "September" => "9月",
-  "October" => "10月",
+  "October" => "10月",   //10月が数値になっているため、文字列に修正
   "November" => "11月",
-  "December" => "12月"];
+  "December" => "12月"   //{}を[]に修正
+  ];
 
 // 12月を表示する
 echo $calendar_2018["December"];
